fe1e9ba1d7552672da966f7b8609ef4b84bde9f3
Boyang Shang <33989408+boyangshang@users.noreply.github.com>
2022-05-14 11:23:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages('FADS','FADS.*'),
+    packages=find_packages(include=['FADS', 'FADS.*']),
     include_package_data=True,
     install_requires=["numpy", "scikit-learn"],
 )
